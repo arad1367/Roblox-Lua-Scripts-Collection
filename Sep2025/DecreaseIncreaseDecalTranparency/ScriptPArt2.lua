fc20201917d4b89fb0cc29db6546d89c729bbcad
Pejman <pejman.ebrahimi77@gmail.com>
2025-09-15 09:57:13 +0200

    DecreaseIncreaseDecalTranparency added

---
+++ Sep2025/DecreaseIncreaseDecalTranparency/ScriptPArt2.lua
@@ -0,0 +1,19 @@
+-- DecreaseIncreaseDecalTranparency --
+
+local Part = game.Workspace.Part2
+local Decal = script.Parent.Decal
+local IsContinue = true
+
+repeat
+
+	while Decal.Transparency < 1 and IsContinue == true do
+		Decal.Transparency += 0.1
+		wait(0.1)
+	end
+
+	while Decal.Transparency > 0 and IsContinue == true do
+		Decal.Transparency -= 0.1
+		wait(0.1)
+	end
+
+until false
